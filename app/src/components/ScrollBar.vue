4cbfcc8828d4353cc9a231c30072e9d9bee2e30c
zhangming <KFT_zhangming@bingosoft.net>
2019-06-12 15:47:49 +0800

    注释

--- app/src/components/ScrollBar.vue
+++ app/src/components/ScrollBar.vue
@@ -1,31 +1,35 @@
 <template>
   <div class="ScrollBar" id="ScrollBar" ref="ScrollBar">
-    <img :src="htmlUrl">
+    <img :src="htmlUrl" oncontextmenu="return false;" ondragstart="return false;" />
   </div>
 </template>
 
 <script>
 import html2canvas from "html2canvas"
+
 export default {
   name: "ScrollBar",
+  props:["el"],
   components: {
     html2canvas
+  },
+  watch:{
+
   },
   data: () => ({
     htmlUrl: ""
   }),
   mounted() {
-   this.$nextTick(() => {
-     this.toImage()
-   })
+    this.$nextTick(() => {
+      this.toImage()
+    })
   },
   methods: {
     // 页面元素转图片
     toImage() {
-      // 第一个参数是需要生成截图的元素,第二个是自己需要配置的参数,宽高等
-      html2canvas(this.$refs.root, {
+      html2canvas(document.getElementById(this.el), {
         backgroundColor: null,
-        useCORS: true // 如果截图的内容里有图片,可能会有跨域的情况,加上这个参数,解决文件跨域问题
+        useCORS: true,
       }).then(canvas => {
         let url = canvas.toDataURL("image/png");
         this.htmlUrl = url;
@@ -35,6 +39,18 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.ScrollBar {
+  position: absolute;
+  right:0px;
+  top:0px;
+  height: 100%;
+  width:6%;
+  border:1px solid red;
+  box-sizing: border-box;
+  img {
+    display: block;
+    width:100%;
+  }
+}
 </style>
--- app/src/views/Home/Home.vue
+++ app/src/views/Home/Home.vue
@@ -1,28 +1,51 @@
 <template>
-  <div class="home" ref="root" id="root">
-    12312
+  <div class="home" id="root">
+    <div class="text-view" id="view">
+      <p v-for="(item,index) of list" :key="index">{{item}}</p>
+    </div>
+    <scroll-bar el="view" ref="ScrollBar"></scroll-bar>
   </div>
 </template>
 
 <script>
+import ScrollBar from "@/components/ScrollBar.vue";
 
 export default {
   name: "home",
-  components: {
-  },
+  components: { ScrollBar },
   data: () => ({
-    htmlUrl: ""
+    htmlUrl: "",
+    list: ["111111111111111111111111111111111111111111"]
   }),
   mounted() {
-   
+    var _this = this;
+    setInterval(function() {
+      _this.update(23);
+    }, 1000);
   },
   methods: {
-    
+    $update(fn) {
+      var _this = this;
+      var _fn = fn;
+      return (function() {
+        _this.$refs.ScrollBar.toImage();
+        _fn.call(_this);
+      })();
+    },
+    update(args) {
+      this.$update(function() {
+        console.log(args);
+      });
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
 @import "~@/static/css/Home/Home.scss";
+#root {
+  font-size: 50px;
+  font-weight: bold;
+}
 </style>
 
